8c44c2e0cd9a78093384824c2290aaada92040be
Krzychu159 <krzysztof.rak159@gmail.com>
2025-03-06 19:08:03 +0100

    Aktualizacja

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -36,38 +36,54 @@ export const Form = ({ onAddOperation }) => {
         Wpłata
       </div>
       <div id="formData">
-        <label htmlFor="name">Nazwa </label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-          required
-        />
-        <label htmlFor="amount">Kwota </label>
-        <input
-          type="number"
-          id="amount"
-          value={amount}
-          onChange={(e) => setAmount(Number(e.target.value))}
-        />
-        <label htmlFor="category">Kategorira </label>
-        <input
-          type="text"
-          id="category"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        />
-        <label htmlFor="date">Data </label>
-        <input
-          type="date"
-          id="category"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-        />
-        <button>Dodaj</button>
+        <div className="formElement">
+          <div className="inputDiv">
+            <label htmlFor="name">Nazwa </label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              required
+            />
+          </div>
+          <div className="inputDiv">
+            <label htmlFor="category">Kategorira </label>
+            <input
+              type="text"
+              id="category"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="formElement">
+          <div className="inputDiv">
+            <label htmlFor="amount">Kwota </label>
+            <input
+              type="number"
+              id="amount"
+              style={{ minWidth: "150px" }} /* 🔹 Minimalna szerokość pola */
+              value={amount}
+              onChange={(e) => setAmount(Number(e.target.value))}
+            />
+          </div>
+          <div className="inputDiv">
+            <label htmlFor="date">Data </label>
+            <input
+              type="date"
+              id="category"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
+          <div className="inputDiv">
+            <label> &nbsp;</label>
+            <button>Dodaj</button>
+          </div>
+        </div>
       </div>
     </form>
   );
